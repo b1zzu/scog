cc9d30e46944a42140928e1b6f8fae3797b82d23
Davide Bizzarri <davide.bizzarri@wuerth-phoenix.com>
2018-04-03 22:14:24 +0200

    Start parsing the main options
    --help
    --config FILE

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let mut config: String = String::from("config.toml");
+
+    // first ( 0 ) arguments is the name of the program
+    let mut i: usize = 1;
+    while i < args.len() {
+        if args[i] == String::from("--help") {
+            help();
+        } else if args[i] == String::from("--config") {
+            if (i + 1) < args.len() {
+                i = i + 1;
+                config = args[i].clone();
+            }
+        }
+        i = i + 1;
+    }
+
+    println!("config: {}", config);
+}
+
+fn help() {
+    println!("Usage: sync [--config FILE]");
+    exit(0);
 }
