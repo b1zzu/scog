7dd473f021869b3ed0a25dc8f2aadf44efbd6b84
Davide Bizzarri <davide.bizzarri@wuerth-phoenix.com>
2018-04-06 12:02:35 +0200

    Extracted config logic in new module

---
+++ src/config.rs
@@ -0,0 +1,41 @@
+use std::collections;
+use std::fs;
+use std::io::Read;
+use std::path;
+use std::process;
+use toml;
+
+#[derive(Deserialize)]
+pub struct Config {
+    pub main: Main,
+    pub files: collections::HashMap<String, File>,
+}
+
+#[derive(Deserialize)]
+pub struct Main {
+    pub repository: String,
+}
+
+#[derive(Deserialize)]
+pub struct File {
+    pub file: String,
+    pub owner: String,
+}
+
+pub fn from(file: &String) -> Config {
+
+    // Test config file
+    let config = path::Path::new(file);
+    if !config.is_file() {
+        println!("sync: error: config file: '{}' does not exists", config.to_string_lossy());
+        process::exit(1);
+    }
+
+    // Open and read config file
+    let mut config = fs::File::open(config).unwrap();
+    let mut contents = String::new();
+    config.read_to_string(&mut contents).unwrap();
+
+    // Parse toml config file to Config struct
+    return toml::from_str(contents.as_str()).unwrap();
+}
