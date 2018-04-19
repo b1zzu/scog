6af29cec8977ac0cc1c1ac47756587d6f104a4c5
Davide Bizzarri <davide.bizzarri@wuerth-phoenix.com>
2018-04-19 17:57:56 +0200

    Cluster tentative

--- src/controller.rs
+++ src/controller.rs
@@ -17,3 +17,11 @@ pub fn end<'a, A: 'a, E: 'a>(method: fn(A) -> Result<A, E>) -> Box<'a + Fn(A) ->
         method(a)
     })
 }
+
+pub fn cluster<'a, A: 'a, E: 'a>(mut methods: Vec<fn(A) -> Result<A, E>>) -> Box<'a + Fn(A) -> Result<A, E>> {
+    let mut next = end(methods.pop().unwrap());
+    while let Some(method) = methods.pop() {
+        next = step(method, next);
+    }
+    next
+}
